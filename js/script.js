f39262c405d201c5aa5515be4a3a5ef60d1b5250
HasegawaTaizou <hasegawataizou86@gmail.com>
2022-08-14 12:22:45 -0300

    60% pronto. To cansadasso, mano

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-let ul = document.querySelector('.personagens')
-ul.classList.add('azul')
+let ul = document.querySelector(".personagens");
+ul.classList.add("azul");
 
 function mudarEstado(elemento) {
-    var display = document.getElementById(elemento).style.display;
-    if(display == "none") {
-        document.getElementById(elemento).style.display = 'block';
-    }
-    else {
-        document.getElementById(elemento).style.display = 'none';
-    }
+  var display = document.getElementById(elemento).style.display;
+  if (display == "block") {
+    document.getElementById(elemento).style.display = "none";
+  } else {
+    document.getElementById(elemento).style.display = "block";
+  }
 }
-
-
