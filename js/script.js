d7231b75a77df726a9082e88657aeaf0845dc6a9
HasegawaTaizou <hasegawataizou86@gmail.com>
2022-08-14 17:05:56 -0300

    Tentativa frustrada de fazer o slider, mas pora, fiquei o dia inteiro fazer o site e FINALMENTE terminei krl. 100%

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-let ul = document.querySelector(".personagens");
-ul.classList.add("azul");
-
 function mudarEstado(elemento) {
-  var display = document.getElementById(elemento).style.display;
+  let display = document.getElementById(elemento).style.display;
   if (display == "block") {
     document.getElementById(elemento).style.display = "none";
   } else {
